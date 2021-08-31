df70dc1d9c23d62db4aa9158bbe754a9cc443ee2
YuliaJahns <chernula2010@mail.ru>
2021-08-31 16:36:26 +0200

    Version2

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # HomeWork18.2
 Public view of my bot
+Мой телеграмбот по переводу нескольких валют в рубли. 
+Ссылка на бот t.me/ExchangeBobaBot
---
+++ config.py
@@ -0,0 +1,7 @@
+TOKEN = "1835072351:AAEsVj3Ne8X_DiZMZ4F_787IobwzGjzQZY0"
+
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+}
